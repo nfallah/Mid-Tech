d8c13919335c47cde5ebf2a5a0d1e239182bf5a2
Nima Fallah <nfallah399@gmail.com>
2023-06-14 03:18:53 -0400

    Further worked on screen element generation

--- Main/Assets/Scripts/ScreenElement.cs
+++ Main/Assets/Scripts/ScreenElement.cs
@@ -7,7 +7,9 @@ public class ScreenElement : MonoBehaviour
 {
     private enum Side { LEFT, RIGHT, DOWN, UP, BACK, FORWARD }
 
-    [SerializeField] float height;
+    [SerializeField] float physicalHeight;
+
+    [SerializeField] int startingHeight; // Calculated from the top of the texture, also measured natively (e.g. pixels over Unity units)
 
     [SerializeField] Side side;
 
@@ -15,7 +17,13 @@ public class ScreenElement : MonoBehaviour
 
     [SerializeField] Texture2D texture;
 
-    [SerializeField] Vector2 offset;
+    [SerializeField] Vector3 offset;
+
+    private float currentHeightRatio, heightRatio;
+
+    private int currentHeight, maxClampHeight, maxHeight;
+
+    private Mesh mesh;
 
     private Vector2 dimensions;
 
@@ -23,7 +31,34 @@ public class ScreenElement : MonoBehaviour
 
     private void Start()
     {
-        dimensions = new Vector2(texture.width / 1000f, height);
+        /*
+         * Ensures the physical height does not convert into a decimal pixel height.
+         * E.g. "1.681" is fine as its pixel height is "1681", but no additional decimals would be allowed past this point.
+         */
+        if (physicalHeight * 1000 % 1 >= 0.0001)
+        {
+            throw new Exception("Physical height cannot have decimals; terminating");
+        }
+
+        // If the physical screen has a larger display than the entire texture, the script terminates.
+        if (texture.height < (int)(physicalHeight * 1000))
+        {
+            Debug.Log(texture.height);
+            Debug.Log(physicalHeight * 1000);
+            throw new Exception("Texture pixel height smaller than the physical height of the screen; terminating.");
+        }
+
+        maxClampHeight = texture.height - (int)(physicalHeight * 1000);
+
+        // Ensures the starting pixel height is not invalid, i.e. the display does not break.
+        if (startingHeight > maxClampHeight)
+        {
+            throw new Exception("Starting pixel height is larger than the maximum pixel height; terminating.");
+        }
+
+        heightRatio = physicalHeight / (texture.height / 1000f);
+        maxHeight = texture.height;
+        dimensions = new Vector2(texture.width / 1000f, physicalHeight);
         CreateScreen();
     }
 
@@ -32,30 +67,42 @@ public class ScreenElement : MonoBehaviour
         GameObject screen = new GameObject(screenName);
 
         screen.transform.parent = transform;
-        screen.transform.localPosition = Vector3.zero;
+        screen.transform.localPosition = offset;
+        mesh = new Mesh();
 
         MeshFilter meshFilter = screen.AddComponent<MeshFilter>();
-        Mesh mesh = new Mesh();
         MeshRenderer meshRenderer = screen.AddComponent<MeshRenderer>();
         Material material = new Material(Shader.Find("Standard"));
 
         material.SetFloat("_Glossiness", 0);
         material.SetTexture("_MainTex", texture);
         meshRenderer.material = material;
-
         mesh.vertices = GetVertices();
         mesh.normals = GetNormals();
         mesh.triangles = triangles;
-        mesh.uv = new Vector2[]
-        {
-            new Vector2(0, 0),
-            new Vector2(0, 1),
-            new Vector2(1, 1),
-            new Vector2(1, 0)
-        };
-
+        currentHeight = startingHeight;
+        mesh.uv = GetUV();
         mesh.RecalculateBounds();
         meshFilter.mesh = mesh;
+    }
+
+    public void Scroll(int amount)
+    {
+        currentHeight = Mathf.Clamp(currentHeight + amount, 0, maxClampHeight);
+        mesh.uv = GetUV();
+    }
+
+    private Vector2[] GetUV()
+    {
+        currentHeightRatio = (float)currentHeight / maxHeight;
+
+        return new Vector2[]
+        {
+            new Vector2(0, 1 - currentHeightRatio - heightRatio),
+            new Vector2(0, 1 - currentHeightRatio),
+            new Vector2(1, 1 - currentHeightRatio),
+            new Vector2(1, 1 - currentHeightRatio - heightRatio)
+        };
     }
 
     private Vector3[] GetNormals()
@@ -96,36 +143,48 @@ public class ScreenElement : MonoBehaviour
             case Side.LEFT:
                 return new Vector3[]
                 {
-
+                    new Vector3(0, 0, 0),
+                    new Vector3(0, dimensions.y, 0),
+                    new Vector3(0, dimensions.y, -dimensions.x),
+                    new Vector3(0, 0, -dimensions.x)
                 };
             case Side.RIGHT:
                 return new Vector3[]
                 {
-
+                    new Vector3(0, 0, 0),
+                    new Vector3(0, dimensions.y, 0),
+                    new Vector3(0, dimensions.y, dimensions.x),
+                    new Vector3(0, 0, dimensions.x)
                 };
             case Side.DOWN:
                 return new Vector3[]
                 {
-
+                    new Vector3(0, 0, 0),
+                    new Vector3(0, 0, dimensions.y),
+                    new Vector3(-dimensions.x, 0, dimensions.y),
+                    new Vector3(-dimensions.x, 0, 0)
                 };
             case Side.UP:
                 return new Vector3[]
                 {
-
+                    new Vector3(0, 0, 0),
+                    new Vector3(0, 0, dimensions.y),
+                    new Vector3(dimensions.x, 0, dimensions.y),
+                    new Vector3(dimensions.x, 0, 0)
                 };
             case Side.BACK:
                 return new Vector3[]
                 {
-                    new Vector3(dimensions.x, 0, 0),
-                    new Vector3(dimensions.x, dimensions.y, 0),
-                    new Vector3(0, dimensions.y , 0),
-                    new Vector3(0, 0, 0)
+                    new Vector3(0, 0, 0),
+                    new Vector3(0, dimensions.y, 0),
+                    new Vector3(-dimensions.x, dimensions.y, 0),
+                    new Vector3(-dimensions.x, 0, 0)
                 };
             case Side.FORWARD:
                 return new Vector3[]
                 {
                     new Vector3(0, 0, 0),
-                    new Vector3(0, dimensions.y , 0),
+                    new Vector3(0, dimensions.y, 0),
                     new Vector3(dimensions.x, dimensions.y, 0),
                     new Vector3(dimensions.x, 0, 0)
                 };
